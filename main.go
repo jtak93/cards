a989c382477c90037c51edc05df6c0916a17b0ce
james tak <jtak93@gmail.com>
2018-11-10 11:14:55 -0800

    adds shuffle

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	cards := newDeckFromFile("my_cards")
+	fmt.Println(cards)
 }
